5d508dad8bb23913b3be38205de045beccebda28
novli <novli@overplay.pro>
2018-02-20 13:30:57 +0300

    + react

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 2. ~~eslint~~
 3. ~~webpack~~
 4. ~~babel~~
-5. react
+5. ~~react~~
 6. hmr
 7. redux
 8. API
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
-import world from 'dummy';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const hello = world;
-console.log(hello);
+import Components from 'Components';
+
+const element = document.getElementById('App');
+
+const renderApp = (App = Components) => {
+  ReactDOM.render(
+    <App />,
+    element,
+  );
+};
+
+renderApp();
